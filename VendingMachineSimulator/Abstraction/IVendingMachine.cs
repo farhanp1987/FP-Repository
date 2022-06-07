50435a7b17d85ff8fe0fcb7c4845b6fecbd721cf
Farhan Patel <106923580+farhanp1987@users.noreply.github.com>
2022-06-07 11:34:15 +0530

    Add files via upload

---
+++ VendingMachineSimulator/Abstraction/IVendingMachine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    interface IVendingMachine
+    {
+        public void DisplayMachineBalance();
+        public void ShowProducts();
+        public void RefillMachine();
+        public void SelectProduct();
+        public void DisplaySaleRecords();
+        public void CancelTransaction();
+        public void ChangeLanguage();
+
+    }
+}
