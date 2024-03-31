ed36efd10eaea14ee4b261bf0303c084afbaabc8
Farhan Patel <106923580+farhanp1987@users.noreply.github.com>
2024-04-01 02:45:02 +0530

    Add files via upload

---
+++ SampleAppInsertUpdate/Models/StringModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleAppInsertUpdate.Models
+{
+    public class StringModel
+    {
+        public int ID { get; set; }
+        public string Value { get; set; }
+        public string UpdatedOn { get; set; }
+
+    }
+}
